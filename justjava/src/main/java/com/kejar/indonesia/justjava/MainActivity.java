cb240865ce25f9eeba3425c772b8b95e1db5fae3
fajarpnugroho <fajar@kurio.co.id>
2016-11-29 18:16:42 +0700

    Added action to view

--- justjava/src/main/java/com/kejar/indonesia/justjava/MainActivity.java
+++ justjava/src/main/java/com/kejar/indonesia/justjava/MainActivity.java
@@ -1,7 +1,9 @@
 package com.kejar.indonesia.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void displayOrder(View view) {
+        updateQty(1);
+    }
+
+    private void updateQty(int order) {
+        TextView qtyView = (TextView) findViewById(R.id.quantity_text_view);
+        qtyView.setText(String.valueOf(order));
     }
 }
