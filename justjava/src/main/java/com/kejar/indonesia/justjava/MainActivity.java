1b8e04de8f5a73b6abbe7a915ea07f5f06572d60
fajarpnugroho <fajar@kurio.co.id>
2016-11-29 17:41:14 +0700

    Initialize project

---
+++ justjava/src/main/java/com/kejar/indonesia/justjava/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kejar.indonesia.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
