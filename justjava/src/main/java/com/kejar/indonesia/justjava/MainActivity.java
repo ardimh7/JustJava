7640226793d86eb54e9dec3eb0334aeb1c844579
fajarpnugroho <fajar@kurio.co.id>
2016-11-29 20:39:14 +0700

    Refactor code

--- justjava/src/main/java/com/kejar/indonesia/justjava/MainActivity.java
+++ justjava/src/main/java/com/kejar/indonesia/justjava/MainActivity.java
@@ -3,22 +3,27 @@ package com.kejar.indonesia.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void displayOrder(View view) {
-        updateQty(1);
+        Button button = (Button) findViewById(R.id.order_button);
+        button.setOnClickListener(this);
     }
 
     private void updateQty(int order) {
         TextView qtyView = (TextView) findViewById(R.id.quantity_text_view);
         qtyView.setText(String.valueOf(order));
+    }
+
+    @Override
+    public void onClick(View view) {
+        updateQty(1);
     }
 }
